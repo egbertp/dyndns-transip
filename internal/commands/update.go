c0ece4176fd021f5613746651dcb6b674cd4d107
Egbert Pot <egbert@egbertpot.nl>
2020-11-29 22:21:55 +0100

    Added newline at the end of output

--- internal/commands/update.go
+++ internal/commands/update.go
@@ -26,10 +26,10 @@ func Update(cmd *cobra.Command, args []string) {
 	)
 	changed, err := tld.UpdateRecord(IP)
 	if err != nil {
-		logger.Get().Fatalf("Unable to create record. (%s)", err.Error())
+		logger.Get().Fatalf("Unable to create record. (%s)\n", err.Error())
 	} else if changed {
-		logger.Get().Infof("Updating record for %s.%s with ip %s.", config.Get().GetString("domain-entry"), config.Get().GetString("domain"), IP.IP)
+		logger.Get().Infof("Updating record for %s.%s with ip %s.\n", config.Get().GetString("domain-entry"), config.Get().GetString("domain"), IP.IP)
 	} else {
-		logger.Get().Infof("Record is up to date %s.%s with ip %s.", config.Get().GetString("domain-entry"), config.Get().GetString("domain"), IP.IP)
+		logger.Get().Infof("Record is up to date %s.%s with ip %s.\n", config.Get().GetString("domain-entry"), config.Get().GetString("domain"), IP.IP)
 	}
 }
